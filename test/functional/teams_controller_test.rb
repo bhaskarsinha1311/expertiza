7534886660607eab471b098cad1223c09a7afe9e
Ajay Pendyala <ajaychandrapendyala@gmail.com>
2016-10-22 00:56:14 -0400

    team controller test file added

--- test/functional/teams_controller_test.rb
+++ test/functional/teams_controller_test.rb
@@ -1,2 +1,25 @@
-class Teams_Controller_Test
+
+require File.dirname(__FILE__) + '/../test_helper'
+require 'teams_controller'
+
+
+# Re-raise errors caught by the controller.
+class Teams_Controller_Test; def rescue_action(e) raise e end; end
+
+
+class Teams_Controller_Test < ActionController::TestCase
+
+	# fixtures for the various objects involved in student task controller
+  fixtures :users, :roles, :participants, :assignments, :due_dates, :deadline_types, :teams
+  # the setup subs 
+
+  def delete_non_existent_team
+
+  end
+
+ 
+
+
+
+
 end
