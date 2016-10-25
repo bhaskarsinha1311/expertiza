1c5fdbd41d24b4f1f554502a200872e1c0640ece
Ajay Pendyala <ajaychandrapendyala@gmail.com>
2016-10-25 02:47:27 -0400

    first test delete

--- test/functional/teams_controller_test.rb
+++ test/functional/teams_controller_test.rb
@@ -9,13 +9,24 @@ class Teams_Controller_Test; def rescue_action(e) raise e end; end
 
 class Teams_Controller_Test < ActionController::TestCase
 
-	# fixtures for the various objects involved in student task controller
+  # fixtures for the various objects involved in student task controller
   fixtures :users, :roles, :participants, :assignments, :due_dates, :deadline_types, :teams
   # the setup subs 
 
-  def delete_non_existent_team
+  test "delete assignment team"  do
+  	
+   team = Team.find(params[:assignment1])
+    
+    delete @team
+    team = Team.find(params[:assignment1])
+
+    assert_not team
 
   end
+
+
+
+  
 
  
 
